1ee6cfb5d22f9d515ebb8c237ae6a8f3c5fcffe5
ricardocanastro <ricardocanastro@24fc1bbc-cd77-11de-91bf-0503b81643b9>
2009-11-09 22:30:19 +0000

    JavaExcelAPI Added.

--- Airport/test/Main.java
+++ Airport/test/Main.java
@@ -1,5 +1,31 @@
 package test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import jxl.*;
+import jxl.read.biff.BiffException;
+
 public class Main {
+
+	public static void main(String args[]) throws BiffException, IOException {
+		Workbook flightsFile = Workbook
+				.getWorkbook(new File(
+						"/home/canastro/Documentos/FEUP/AIAD/FLIGHTS_2009_09_Planeado_Real.xls"));
+		
+		Sheet sheet = flightsFile.getSheet(0); 
+		
+		Cell a1 = sheet.getCell(0,0);
+		Cell b2 = sheet.getCell(1,1);
+		Cell c2 = sheet.getCell(2,1);
+
+		String stringa1 = a1.getContents();
+		String stringb2 = b2.getContents();
+		String stringc2 = c2.getContents(); 
+		
+		System.out.println(stringa1+" "+stringb2+" "+stringc2);
+		
+		flightsFile.close();
+	}
 
 }
